d564c6aaca9fa52c683f53f711f0d2843b392bcb
Ivan Mendoza <ivan.menramos@gmail.com>
2025-11-17 22:55:36 -0600

    Segundo commit

--- PROCESO_EXTRACCION.py
+++ PROCESO_EXTRACCION.py
@@ -18,14 +18,3 @@ def extraer_datos(pais):
     datos = respuesta.json()
     return datos
 
-#extraccion = extraer_datos("mexico")
-#print(extraccion)
-
-    
-	
-"""pais = "mexico"
-url = f"https://restcountries.com/v3.1/name/{pais}"
-respuesta = requests.get(url)
-datos = respuesta.json()
-#print(datos[0]["name"]["common"])
-print(datos)"""
